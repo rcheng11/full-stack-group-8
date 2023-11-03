bab1351c087441939fe5bcf62a0088c6bbdb927f
Denny Zhang <dennyzhang@Dennys-MacBook-Air.local>
2023-11-03 11:59:09 -0400

    Login page made
    Made initial login page from watching a youtube video:https://www.youtube.com/watch?v=hlwlM4a5rxg. This is just to see what style we would like. It will be updated later.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public")); 
 
 app.use(bodyParser.urlencoded({extended:true}))
 
