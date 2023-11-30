8a92aaf2be41309d3e3ae045e9f8cdd316542f86
Lyribox <84852274+Lyribox@users.noreply.github.com>
2023-11-30 12:54:15 -0500

    Reconfigured links in index and profile

---
+++ public/JS/profile.js
@@ -0,0 +1,7 @@
+document.getElementById("pfp-create-btn").onclick = function() {
+    window.location.href = "/create";
+};
+
+document.getElementById("pfp-challenge-btn").onclick = function() {
+    window.location.href = "/inbox";
+};
